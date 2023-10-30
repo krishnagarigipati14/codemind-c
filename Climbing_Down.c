d3eb96ad2e421d15164f6c4e9964115930c12f04
krishnawriter <147475905+krishnawriter@users.noreply.github.com>
2023-10-30 11:26:33 +0530

    #include <stdio.h> int main(){ 	int a,b,x,y; 	scanf("%d%d%d%d", &a, &b, &x, &y);
    	int c,d;
    	
    	if(a/x==0){
    		c = a/x;
    	}
    	
    	else{
    		c = (a/x)+1;
    	}
    	
    	if(b/y==0){
    		d = b/y;
    	}
    	
    	else{
    		d = (b/y)+1;
    	}
    	
    	if(c>d){
    		printf("Jesse");
    	}
    	
    	else if(d>c){
    		printf("Walter");
    	}
    	else if(a<b){
    		printf("Walter");
    	}
    	
    	else if(b<a){
    		printf("Jesse");
    	}
    	else{
    		
    		printf("Both");
    	}
    	
    	
    	
    }

---
+++ Climbing_Down.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+int main(){
+	int a,b,x,y;
+	scanf("%d%d%d%d", &a, &b, &x, &y);
+	
+	int c,d;
+	
+	if(a/x==0){
+		c = a/x;
+	}
+	
+	else{
+		c = (a/x)+1;
+	}
+	
+	if(b/y==0){
+		d = b/y;
+	}
+	
+	else{
+		d = (b/y)+1;
+	}
+	
+	if(c>d){
+		printf("Jesse");
+	}
+	
+	else if(d>c){
+		printf("Walter");
+	}
+	else if(a<b){
+		printf("Walter");
+	}
+	
+	else if(b<a){
+		printf("Jesse");
+	}
+	else{
+		
+		printf("Both");
+	}
+	
+	
+	
+}
