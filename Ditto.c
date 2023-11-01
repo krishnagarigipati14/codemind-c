0af9a409506cd11158d2091b3298afbba13c55e2
krishnawriter <147475905+krishnawriter@users.noreply.github.com>
2023-11-01 20:58:26 +0530

    #include <stdio.h> #include <stdlib.h> int main(){     int x,y,p;     scanf("%d%d%d", &x, &y, &p);
        if((x-y)%(2*p)==0){
            printf("YES");
        }
        
        else{
            printf("NO");
        }
        
        return 0;
    }

---
+++ Ditto.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int x,y,p;
+    scanf("%d%d%d", &x, &y, &p);
+    
+    if((x-y)%(2*p)==0){
+        printf("YES");
+    }
+    
+    else{
+        printf("NO");
+    }
+    
+    return 0;
+}
